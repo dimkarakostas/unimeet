80ef6da42dc2829bcbc72f222da3ed103472ffa4
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-26 19:31:24 +0200

    Add initial base realtime service

---
+++ realtime/index.js
@@ -0,0 +1,21 @@
+const program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-p, --port <port>', 'specify the websocket port to listen to [3031]', 3031)
+    .parse(process.argv);
+
+
+const io = require('socket.io'),
+      winston = require('winston');
+
+winston.level = 'debug';
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {'timestamp': true});
+
+const PORT = program.port;
+
+winston.info('Unichat realtime service');
+winston.info('Listening on port ' + PORT);
+
+const socket = io.listen(PORT);
