68ab4ef85f181fb19f5f662bd2e445a3cd98e90d
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-28 20:26:13 +0200

    Add email field/index to User

--- registrar/models/user.js
+++ registrar/models/user.js
@@ -2,7 +2,19 @@
 
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        username: DataTypes.STRING
+        username: DataTypes.STRING,
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+    }, {
+        indexes: [
+            {unique: true, fields: ['email']}
+        ],
+    });
     });
 
     return User;
