6145a3500f81fc2ade3cec9eb083220f8679d025
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-28 14:57:18 +0200

    Connect to db from registrar index

--- registrar/index.js
+++ registrar/index.js
@@ -7,7 +7,8 @@ program
 
 
 const io = require('socket.io'),
-      winston = require('winston');
+      winston = require('winston'),
+      models = require('./models');
 
 winston.level = 'debug';
 winston.remove(winston.transports.Console);
@@ -16,3 +17,9 @@ winston.add(winston.transports.Console, {'timestamp': true});
 const PORT = program.port;
 
 winston.info('Unichat registrar service');
+winston.info('Connecting to database...');
+
+models.sequelize.sync().then( () => {
+    winston.info('Listening on port ' + PORT);
+    const socket = io.listen(PORT);
+});
