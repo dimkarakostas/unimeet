495cb7e56fe64f19bec93f36314827e0d992a670
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-28 20:28:34 +0200

    Add addUser function

--- registrar/index.js
+++ registrar/index.js
@@ -19,6 +19,36 @@ const PORT = program.port;
 winston.info('Unichat registrar service');
 winston.info('Connecting to database...');
 
+const addUser = (user) => {
+    models.User.create(user).then(() => {
+        winston.debug('User ' + user.username + ' has been created.');
+    }).catch((e) => {
+        if (e.name == 'SequelizeValidationError') {
+            if (e.message.startsWith("notNull Violation")) {
+                winston.error(user.username + ': Please fill all necessary fields.');
+            }
+            else if (e.message.startsWith("Validation error: Validation isEmail failed")) {
+                winston.error(user.username + ': Invalid email given.');
+            }
+            else if (e.message.startsWith("Password confirmation")) {
+                winston.error(user.username + ': Passwords don\'t match');
+            }
+            else if (e.message.startsWith("Password should be at least 6 characters")) {
+                winston.error(user.username + ': ' + e.message);
+            }
+            else {
+                throw e;
+            }
+        }
+        else if (e.name == 'SequelizeUniqueConstraintError') {
+            winston.error(user.email + ': Email already exists.');
+        }
+        else {
+            throw e;
+        }
+    });
+};
+
 models.sequelize.sync().then( () => {
     winston.info('Listening on port ' + PORT);
     const socket = io.listen(PORT);
