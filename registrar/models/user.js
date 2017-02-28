6a0f37532e7295ca499c42d78f4d38a4defc1bfb
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-28 21:34:14 +0200

    Add sex field to User

--- registrar/models/user.js
+++ registrar/models/user.js
@@ -30,6 +30,11 @@ module.exports = function(sequelize, DataTypes) {
         },
         school: DataTypes.STRING,
         university: DataTypes.STRING,
+        sex: {  // false -> male, true -> female
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
         isChatting: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
