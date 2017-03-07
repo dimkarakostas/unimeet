ec83c98ab594dfe5b49266d037dd364645fb46f3
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-03-07 18:43:46 +0200

    Use forceNew in realtime client sockets

--- frontend/realtime_client/realtime_client.jsx
+++ frontend/realtime_client/realtime_client.jsx
@@ -9,7 +9,7 @@ const UnichatClient = {
     _canSendMessages: false,
     init(realtimeUrl) {
         this._realtimeUrl = realtimeUrl;
-        this._socket = io.connect(this._realtimeUrl);
+        this._socket = io.connect(this._realtimeUrl, {'forceNew': true});
         this._socket.on('connect', () => {
             console.log('Connected');
             this._socket.emit('client-hello');
