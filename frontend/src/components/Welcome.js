52d610cb8c2d14ae222615953d7d079693742e82
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-19 20:13:23 +0300

    Add initial React index/Welcome components

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/index.css';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="Welcome">
+      </div>
+    );
+  }
+}
+
+export default Welcome;
