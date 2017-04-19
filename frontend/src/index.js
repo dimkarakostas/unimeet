f8ad2c54a9f3a7e9e8a360c3f61907718f82772c
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-19 20:26:24 +0300

    Add initial React routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Welcome from './components/Welcome';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 ReactDOM.render(
-    <Welcome />,
+    <Router>
+        <div>
+            <Route exact path='/' component={Welcome} />
+        </div>
+    </Router>,
     document.getElementById('root')
 );
