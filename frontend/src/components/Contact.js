4b0366a6391558239e5e5a1a81fb470a7d71de86
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-20 22:10:38 +0300

    Add initial Contact component

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/contact.css';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="Contact">
+            </div>
+        );
+    }
+}
+
+export default Contact;
