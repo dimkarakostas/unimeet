d4cad14e916535978c9e8d36c9a3dec0abec17a7
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-20 22:11:10 +0300

    Add Topbar to Contact

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Topbar from './Topbar';
 import '../css/contact.css';
 
 class Contact extends Component {
     render() {
         return (
             <div className="Contact">
+                <Topbar page={'contact'} />
             </div>
         );
     }
