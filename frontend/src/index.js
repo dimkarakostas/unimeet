fcb6de8c807f0c523b032744606cc4ace68ef7cf
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-20 22:10:51 +0300

    Add contact route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Welcome from './components/Welcome';
+import Contact from './components/Contact';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
         <div>
             <Route exact path='/' component={Welcome} />
+            <Route exact path='/contact' component={Contact} />
         </div>
     </Router>,
     document.getElementById('root')
