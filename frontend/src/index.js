ed335b735f9a7ece13c85a022ccdb6a986c66f8c
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-21 18:40:36 +0300

    Add Chat route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Welcome from './components/Welcome';
 import Contact from './components/Contact';
+import Chat from './components/Chat';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 ReactDOM.render(
@@ -9,6 +10,7 @@ ReactDOM.render(
         <div>
             <Route exact path='/' component={Welcome} />
             <Route exact path='/contact' component={Contact} />
+            <Route exact path='/chat' component={Chat} />
         </div>
     </Router>,
     document.getElementById('root')
