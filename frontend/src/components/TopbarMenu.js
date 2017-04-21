4c1b895bea8eea3a34a4241c0d029d83479ff67e
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-21 18:43:58 +0300

    Fix TopbarMenu class name, tabs

--- frontend/src/components/TopbarMenu.js
+++ frontend/src/components/TopbarMenu.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap';
 import LoginForm from './LoginForm';
 
-class Topbar extends Component {
+class TopbarMenu extends Component {
     render() {
         return (
-            <div className="Contact">
-            {this.props.page === 'welcome' ?
-                <Navbar.Collapse>
-                    <LoginForm />
-                </Navbar.Collapse> :
-            (this.props.page === 'contact') ?
-                null :
-                null}
+            <div className="TopbarMenu">
+                {this.props.page === 'welcome' ?
+                    <Navbar.Collapse>
+                        <LoginForm />
+                    </Navbar.Collapse> :
+                this.props.page === 'contact' ?
+                    null :
+                    null
+                }
             </div>
         );
     }
 }
 
-export default Topbar;
+export default TopbarMenu;
