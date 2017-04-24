79ff5f02860bdee1f8f17e78fde7758d34cd55dd
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-24 14:53:00 +0300

    Add initial SettingsModalOption component

--- frontend/src/components/SettingsModalOptionsList.js
+++ frontend/src/components/SettingsModalOptionsList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import SettingsModalOption from './SettingsModalOption';
 
 class SettingsModalOptionsList extends Component {
+    getOptions = () => {
+        return this.props.optionsList.map((option) => {
+            return <SettingsModalOption key={option} option={option} active={this.props.active} handleOptionSelection={this.props.handleOptionSelection} />;
+        });
+    }
+
     render() {
         return (
             <div className="col-lg-3 col-md-3 col-sm-3 col-xs-4 modal-tab-menu">
                 <div className="list-group">
+                    {this.getOptions()}
                 </div>
             </div>
         );
