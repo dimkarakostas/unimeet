571d0dd5dd9e284a1213ef2869148693887f3752
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-24 14:54:31 +0300

    Add initial SettingsModalContentList component

--- frontend/src/components/SettingsModal.js
+++ frontend/src/components/SettingsModal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Modal} from 'react-bootstrap';
 import SettingsModalOptionsList from './SettingsModalOptionsList';
+import SettingsModalContentList from './SettingsModalContentList';
 
 class SettingsModal extends Component {
     constructor(props) {
@@ -25,6 +26,7 @@ class SettingsModal extends Component {
                 <Modal.Body>
                     <div className="row">
                         <SettingsModalOptionsList optionsList={this.state.optionsList} active={this.state.active} handleOptionSelection={this.handleOptionSelection} />
+                        <SettingsModalContentList active={this.state.active} options={this.state.optionsList} />
                     </div>
                 </Modal.Body>
             </Modal>
---
+++ frontend/src/components/SettingsModalContentList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SettingsModalContentList extends Component {
+    render() {
+        return (
+            <div className="col-lg-9 col-md-9 col-sm-9 col-xs-8 modal-tab">
+            </div>
+        );
+    }
+}
+
+export default SettingsModalContentList;
