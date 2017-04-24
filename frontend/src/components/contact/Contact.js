84cdbb59f6db90a5ef518f0c44f5ed4799368f34
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-25 00:53:49 +0300

    Move contact CSS to contact folder

--- frontend/src/components/contact/Contact.js
+++ frontend/src/components/contact/Contact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Topbar from '../Topbar';
 import ContactForm from './ContactForm';
 import Footer from '../Footer';
-import '../../css/contact.css';
+import './css/contact.css';
 
 class Contact extends Component {
     render() {
