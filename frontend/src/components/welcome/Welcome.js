c1bcb255a3c45d1008d3adf577ed28fb81cb0d51
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-25 01:46:02 +0300

    Rename welcome CSS to styles

--- frontend/src/components/welcome/Welcome.js
+++ frontend/src/components/welcome/Welcome.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Topbar, Footer} from '../common';
 import Intro from './body/Intro';
-import './css/welcome.css';
+import './styles.css';
 
 class Welcome extends Component {
     render() {
