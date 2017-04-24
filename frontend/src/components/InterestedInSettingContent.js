3a159337f08bebcb529b782f271e37ac426f3e1e
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-24 14:57:41 +0300

    Add interested in form with sex option

--- frontend/src/components/InterestedInSettingContent.js
+++ frontend/src/components/InterestedInSettingContent.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import {Col, Form, FormControl, FormGroup, Button} from 'react-bootstrap';
 
 class InterestedInSettingContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedSex: 'men',
+            isApplyButtonLoading: false,
+            displayChangesSavedMessage: false
+        };
+    }
+
     isActive = () => {
         return 'modal-tab-content ' + ((this.props.active === this.props.option) ? 'active' : '');
+    }
+
+    handleSexChange = (event) => {
+        this.setState({selectedSex: event.target.value});
+    }
+
+    handleSettingChange = (event) => {
+        event.preventDefault();
+        this.setState({isApplyButtonLoading: true, displayChangesSavedMessage: false});
+        setTimeout(() => {
+            this.setState({
+                isApplyButtonLoading: false,
+                displayChangesSavedMessage: true
+            });
+        }, 2000);
+        //TODO: Setting change request to backend IF NECESSARY
+        setTimeout(() => {
+            this.setState({
+                displayChangesSavedMessage: false
+            });
+        }, 5000);
     }
 
     render() {
@@ -10,6 +41,35 @@ class InterestedInSettingContent extends Component {
             <div className={this.isActive()}>
                 <div className="interested-in-setting">
                     <h4>Interested in</h4><hr/>
+                    <Form horizontal onSubmit={this.handleSettingChange}>
+                        <b>I want to talk to:</b>
+                        <FormGroup className="interested-sex">
+                            <Col sm={4}>
+                                <FormControl
+                                    componentClass="select"
+                                    placeholder="select"
+                                    name="sex"
+                                    id="interested-sex"
+                                    defaultValue={this.state.selectedSex}
+                                    onChange={this.handleSexChange}
+                                >
+                                    <option value="whatever">Whatever</option>
+                                    <option value="men">Men</option>
+                                    <option value="women">Women</option>
+                                </FormControl>
+                            </Col>
+                        </FormGroup>
+
+                        <Button
+                            type="submit"
+                            bsStyle="primary"
+                            disabled={this.state.isApplyButtonLoading}
+                            onClick={!this.state.isApplyButtonLoading? this.handleSettingChange: null}
+                        >
+                            {this.state.isApplyButtonLoading? 'Applying changes...' : 'Apply'}
+                        </Button>
+                        {this.state.displayChangesSavedMessage? <b className="changes-saved-message">Your changes were saved.</b> : null}
+                    </Form>
                 </div>
             </div>
         );
