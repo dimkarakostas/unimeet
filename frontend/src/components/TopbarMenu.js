6cabb6b2329c9f2090496a34cb1c40f9eca45065
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-25 01:25:10 +0300

    Move hardcoded username state option to ChatMenu from TopbarMenu

--- frontend/src/components/TopbarMenu.js
+++ frontend/src/components/TopbarMenu.js
@@ -3,13 +3,6 @@ import {WelcomeTopMenu} from './welcome';
 import {ChatTopMenu} from './chat';
 
 class TopbarMenu extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: 'el10179@mail.ntua.gr'
-        };
-    }
-
     render() {
         return (
             <div className="TopbarMenu">
@@ -18,7 +11,7 @@ class TopbarMenu extends Component {
                 this.props.page === 'contact' ?
                     null :
                 this.props.page === 'chat' ?
-                    <ChatTopMenu username={this.state.username} /> :
+                    <ChatTopMenu /> :
                     null
                 }
             </div>
