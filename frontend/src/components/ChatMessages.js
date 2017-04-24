5430fcdd1c0fbbe071ff250f2f5f84d82d6e0419
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-24 18:58:35 +0300

    Add initial ChatMessages component

---
+++ frontend/src/components/ChatMessages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ChatMessages extends Component {
+    render() {
+        return (
+            <div className="ChatMessages">
+                <div className="container panel-body">
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ChatMessages;
--- frontend/src/components/Chatbody.js
+++ frontend/src/components/Chatbody.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import ChatMessages from './ChatMessages';
 
 class Chatbody extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            partner: {
+                gender: 'female',
+                university: 'University of Athens, Greece'
+            },
+            me: {
+                gender: 'undefined',
+                university: 'National Technical University of Athens, Greece'
+            },
+            messages: [
+                {from: 'partner', timestamp: new Date(), content: 'hi, how are you?',},
+                {from: 'me', timestamp: new Date(), content: 'Fine, you?',},
+            ]
+        };
+    }
+
     render() {
         return (
             <div className="Chatbox">
+                <ChatMessages messages={this.state.messages} partner={this.state.partner} me={this.state.me} />
             </div>
         );
     }
