2af96c1b8b57cc464f3f9a855b5661e35f753109
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-04-25 03:04:12 +0300

    Merge TopbarMenu component to Topbar

--- frontend/src/components/common/Topbar.js
+++ frontend/src/components/common/Topbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap';
-import TopbarMenu from './TopbarMenu';
+import {WelcomeTopMenu} from '../welcome';
+import {ChatTopMenu} from '../chat';
 
 class Topbar extends Component {
     render() {
@@ -11,7 +12,14 @@ class Topbar extends Component {
                         <a href="/">Unichat</a>
                     </Navbar.Brand>
                 </Navbar.Header>
-                <TopbarMenu page={this.props.page} />
+                {this.props.page === 'welcome' ?
+                    <WelcomeTopMenu /> :
+                this.props.page === 'contact' ?
+                    null :
+                this.props.page === 'chat' ?
+                    <ChatTopMenu /> :
+                    null
+                }
             </Navbar>
         );
     }
