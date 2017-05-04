d1388a0d1145c022d0cb4431d1b3ce05a2dec677
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-05-04 14:56:11 +0300

    Add initial connector object

---
+++ frontend/src/components/chat/connection/connector.js
@@ -0,0 +1,28 @@
+import io from 'socket.io-client';
+
+function connector(realtimeUrl, roomId, handleNewMessageCallback) {
+    this._roomId = roomId;
+    this._socket = io.connect(realtimeUrl, {'forceNew': true});
+    this._socket.on('connect', () => {
+        this._socket.emit('client-hello');
+    });
+    this._socket.on('server-hello', (client_id) => {
+        this._socket.emit('join-room', {
+            roomId: roomId
+        });
+    });
+    this._socket.on('server-message', (message, from) => {
+        handleNewMessageCallback(message, from);
+    });
+    this._socket.on('disconnect', () => {
+        return;
+    });
+    this.broadcastMessage = function(message) {
+        this._socket.emit('client-message', {
+            roomId: this._roomId,
+            message: message
+        });
+    };
+};
+
+module.exports = connector;
