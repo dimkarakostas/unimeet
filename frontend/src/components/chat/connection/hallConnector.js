2c0b10149323d860398403d2b274ed4bba33cce1
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-05-04 20:13:33 +0300

    Emit client-get-partner on hall client connection

--- frontend/src/components/chat/connection/hallConnector.js
+++ frontend/src/components/chat/connection/hallConnector.js
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 function hallConnector(hallUrl) {
     this._socket = io.connect(hallUrl, {'forceNew': true});
     this._socket.on('connect', () => {
+        this._socket.emit('client-get-partner');
     });
 };
 
