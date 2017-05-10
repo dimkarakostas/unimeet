575843825e43ab003eedc8ad74a1f39de5fdd21a
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-05-10 16:57:31 +0300

    Add repo link to license reference

--- frontend/src/components/chat/navigation/HelpModal.js
+++ frontend/src/components/chat/navigation/HelpModal.js
@@ -29,7 +29,7 @@ class HelpModal extends Component {
                         </div>
                     </div>
                     <hr/>
-                    <div className="copyright text-muted text-center">© 2017 Unichat. MIT licensed, some rights reserved.</div>
+                    <div className="copyright text-muted text-center">© 2017 Unichat. <a href="https://github.com/dimkarakostas/unichat" target="_blank">MIT licensed</a>, some rights reserved.</div>
                 </Modal.Body>
             </Modal>
         );
