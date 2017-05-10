a93d3a1c7c0be3262108f48f672c1f06410f763f
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-05-10 22:04:15 +0300

    Replace hardcoded string with cookie variable in presenceConnector

--- frontend/src/components/chat/connection/presenceConnector.js
+++ frontend/src/components/chat/connection/presenceConnector.js
@@ -1,9 +1,10 @@
 import io from 'socket.io-client';
 
 function presenceConnector(presenceUrl, joinRoomCallback) {
+    this._cookieId = 'cookie';
     this._socket = io.connect(presenceUrl, {'forceNew': true});
     this._socket.on('connect', () => {
-        this._socket.emit('client-get-partner', 'client');
+        this._socket.emit('client-get-partner', this._cookieId);
     });
     this._socket.on('server-join-room', (roomId) => {
         joinRoomCallback(roomId);
