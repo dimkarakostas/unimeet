7617ac1fa87e5a7c68c515c4a7daf4a96177b272
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-06-05 19:31:16 +0300

    Add initial get_schools view

--- backend/unichat/urls.py
+++ backend/unichat/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^get_schools$', views.get_schools, name='get_schools'),
 ]
--- backend/unichat/views.py
+++ backend/unichat/views.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import JsonResponse
+from .models import School
 
-from django.shortcuts import render
 
-# Create your views here.
+def get_schools(request):
+    schools = School.objects.all()
+    res_data = {'schools': []}
+    for s in schools:
+        school = {}
+        school['id'] = s.id
+        school['name'] = s.name
+        school['site'] = s.site
+        school['university'] = s.university.name
+        school['city'] = s.university.city.name
+        school['country'] = s.university.city.country.name
+        res_data['schools'].append(school)
+    resp = JsonResponse(res_data)
+    resp['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
