81fd41e4324696cedb38979758805331b9434fc7
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-06-05 20:08:39 +0300

    Add installation process to start script

--- start.sh
+++ start.sh
@@ -17,12 +17,40 @@ export NVM_DIR=$HOME/.nvm
 nvm use 6.3
 
 echo "[*] Starting Unichat..."
+
 rm backend/db.sqlite3
-backend/env/bin/python backend/manage.py migrate
-backend/env/bin/python backend/initialize_database.py
-backend/env/bin/python backend/manage.py runserver &
+cd backend
+if [ ! -d "env" ]; then
+    virtualenv env
+    env/bin/pip install -r requirements.txt
+fi
+env/bin/python manage.py migrate
+env/bin/python initialize_database.py
+env/bin/python manage.py runserver &
+cd ..
+
+if [ ! -d "realtime/node_modules" ]; then
+    (cd realtime
+    npm install)
+fi
 npm start --prefix realtime &
+
+if [ ! -d "presence/node_modules" ]; then
+    (cd presence
+    npm install)
+fi
 npm start --prefix presence &
+
+if [ ! -d "matchmaker/node_modules" ]; then
+    (cd matchmaker
+    npm install)
+fi
 npm start --prefix matchmaker &
+
+if [ ! -d "frontend/node_modules" ]; then
+    (cd frontend
+    npm install)
+fi
 npm start --prefix frontend &>/dev/null &
+
 wait
