0a22cfa95e3b6153b05ddd1c6c535c72d1998245
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-06-06 23:15:44 +0300

    Add check_signup_email backend view helper

--- backend/unichat/helpers.py
+++ backend/unichat/helpers.py
@@ -1,4 +1,5 @@
 from .models import School
+import re
 
 
 def get_school_list():
@@ -14,3 +15,10 @@ def get_school_list():
         school['country'] = s.university.city.country.name
         school_list.append(school)
     return school_list
+
+
+def check_signup_email(email):
+    for regex in School.objects.all().values_list('mailRegex', flat=True):
+        if re.match(regex, email):
+            return True
+    return False
--- backend/unichat/views.py
+++ backend/unichat/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
-from helpers import get_school_list
+from helpers import get_school_list, check_signup_email
 
 
 def get_schools(request):
@@ -14,6 +14,10 @@ def get_schools(request):
 def signup(request):
     if request.method == "POST":
         signup_parameters = json.loads(request.body.decode('utf-8'))
+        if check_signup_email(signup_parameters['email']):
+            resp = HttpResponse('Signup OK')
+        else:
+            resp = HttpResponseBadRequest('Invalid univesity email')
     elif request.method == "OPTIONS":
         resp = HttpResponse('')
         resp['Access-Control-Allow-Headers'] = 'Content-Type'
