e873ee40ba2b7a183768c4a9d9c81bded35f5bd1
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-06-06 23:14:55 +0300

    Add backend signup view

--- backend/unichat/views.py
+++ backend/unichat/views.py
@@ -1,8 +1,23 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+import json
 from helpers import get_school_list
 
 
 def get_schools(request):
     resp = JsonResponse({'schools': get_school_list()})
+    resp['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == "POST":
+        signup_parameters = json.loads(request.body.decode('utf-8'))
+    elif request.method == "OPTIONS":
+        resp = HttpResponse('')
+        resp['Access-Control-Allow-Headers'] = 'Content-Type'
+    else:
+        resp = HttpResponseBadRequest('')
     resp['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     return resp
