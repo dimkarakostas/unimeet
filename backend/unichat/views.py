80e861bc273cc350e311a95cf62ac6fbb5fd6f65
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-06-14 14:02:59 +0300

    Add login view

--- backend/unichat/views.py
+++ backend/unichat/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
 from helpers import get_school_list, get_school_by_email, create_user
+from django.contrib.auth import authenticate, login as Django_login
 
 
 def get_schools(request):
@@ -21,6 +22,28 @@ def signup(request):
             resp = HttpResponse('Signup OK')
         else:
             resp = HttpResponseBadRequest('Invalid univesity email')
+    elif request.method == "OPTIONS":
+        resp = HttpResponse('')
+        resp['Access-Control-Allow-Headers'] = 'Content-Type'
+    else:
+        resp = HttpResponseBadRequest('Invalid request method')
+    resp['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
+
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        login_parameters = json.loads(request.body.decode('utf-8'))
+        email = login_parameters['email']
+        password = login_parameters['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            Django_login(request, user)
+            resp = HttpResponse('Login OK')
+            resp.set_cookie('unichat_login', 'jim')
+        else:
+            resp = HttpResponseBadRequest('Bad credentials')
     elif request.method == "OPTIONS":
         resp = HttpResponse('')
         resp['Access-Control-Allow-Headers'] = 'Content-Type'
