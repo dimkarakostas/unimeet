409dbd6e8c951b949b0ac3398ac942861b6aff7b
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-06-14 14:03:34 +0300

    Add login url pattern

--- backend/unichat/urls.py
+++ backend/unichat/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^get_schools$', views.get_schools, name='get_schools'),
     url(r'^signup$', views.signup, name='signup'),
+    url(r'^login$', views.login, name='login'),
 ]
