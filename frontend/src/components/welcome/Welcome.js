084387bd188f542c6d9ed8bf132c043572e764a5
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-07-26 19:50:47 +0300

    Redirect to chat from home if user logged in

--- frontend/src/components/welcome/Welcome.js
+++ frontend/src/components/welcome/Welcome.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import {Topbar, Footer} from '../common';
 import Intro from './body/Intro';
 import './styles.css';
+import * as config from '../config';
+
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 class Welcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedOut: false
+        };
+    }
+
+    static get contextTypes() {
+        return {
+            router: React.PropTypes.object.isRequired,
+        };
+    }
+
+    componentDidMount = () => {
+        axios.get(config.backendUrl + '/is_user_logged_in')
+        .then(res => {
+            this.context.router.history.push('/chat');
+        })
+        .catch(error => {
+            this.setState({isLoggedOut: true});
+        })
+    }
+
     render() {
         return (
             <div className="Welcome">
-                <Topbar page={'welcome'} />
-                <Intro />
-                <Footer />
+                {this.state.isLoggedOut ?
+                    <div>
+                        <Topbar page={'welcome'} />
+                        <Intro />
+                        <Footer />
+                    </div>
+                : ''}
             </div>
         );
     }
