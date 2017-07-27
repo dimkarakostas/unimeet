c3f7180c41f2397f1cd9a0a789016b515e27cdf4
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-07-27 19:21:27 +0300

    Get user gender from backend

--- frontend/src/components/chat/navigation/SettingsModalContentList.js
+++ frontend/src/components/chat/navigation/SettingsModalContentList.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import InterestedInSettingContent from './InterestedInSettingContent';
 import PersonalInformationSettingContent from './PersonalInformationSettingContent';
 import AccountSettingContent from './AccountSettingContent';
+import * as config from '../../config';
+
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 class SettingsModalContentList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gender: ''
+        };
+    }
+
+    componentDidMount() {
+        axios.get(config.backendUrl + '/user_info')
+        .then(res => {
+            this.setState({gender: res.data.gender});
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     render() {
         return (
             <div className="col-lg-9 col-md-9 col-sm-9 col-xs-8 modal-tab">
                 <InterestedInSettingContent active={this.props.active} option={this.props.options[0]}/>
-                <PersonalInformationSettingContent active={this.props.active} option={this.props.options[1]} />
+                {this.state.gender !== '' ?
+                    <PersonalInformationSettingContent active={this.props.active} option={this.props.options[1]} gender={this.state.gender} />
+                : ''}
                 <AccountSettingContent active={this.props.active} option={this.props.options[2]} />
             </div>
         );
