75098537d6a36f898775d62be717b9378cd91044
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-08-03 13:04:55 +0300

    Clean start script

--- start.sh
+++ start.sh
@@ -19,34 +19,10 @@ nvm use 6.3
 echo "[*] Starting Unimeet..."
 
 cd backend
-if [ ! -d "env" ]; then
-    virtualenv env
-    env/bin/pip install -r requirements.txt
-    if [ ! -e "db.sqlite3" ]; then
-        rm backend/db.sqlite3
-        env/bin/python manage.py migrate
-        env/bin/python initialize_database.py
-    fi
-fi
 env/bin/uwsgi --ini uwsgi.ini &
 cd ..
-
-if [ ! -d "realtime/node_modules" ]; then
-    (cd realtime
-    npm install)
-fi
 npm start --prefix realtime &
-
-if [ ! -d "presence/node_modules" ]; then
-    (cd presence
-    npm install)
-fi
 npm start --prefix presence &
-
-if [ ! -d "matchmaker/node_modules" ]; then
-    (cd matchmaker
-    npm install)
-fi
 npm start --prefix matchmaker &
 
 wait
