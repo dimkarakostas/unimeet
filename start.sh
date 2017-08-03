609b07a5d4947d2f32dd05451b5ff4677c3a418b
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-08-03 13:43:13 +0300

    Add Nginx prompt to start script

--- start.sh
+++ start.sh
@@ -11,16 +11,25 @@ terminate() {
     echo "[*] Shut down complete."
 }
 
+echo "[*] Starting Unimeet..."
+
+read -p "Have you setup Nginx? (y/n) (default: n): " ENV
+ENV=${ENV:-"n"}
+cd backend
+if [ "$ENV" == "n" ];
+then
+    env/bin/python manage.py runserver &
+else
+    env/bin/uwsgi --ini uwsgi.ini &
+fi
+cd ..
+
+
 export NVM_DIR=$HOME/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 nvm use 6.3
 
-echo "[*] Starting Unimeet..."
-
-cd backend
-env/bin/uwsgi --ini uwsgi.ini &
-cd ..
 npm start --prefix realtime &
 npm start --prefix presence &
 npm start --prefix matchmaker &
